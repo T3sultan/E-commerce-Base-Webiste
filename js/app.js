15d820b3cf327c379a98ffcfe45debb9d80b09b8
“T3sultan” <incognito.soul011@gmail.com>
2021-09-12 23:24:45 +0600

    remove doller show pic and row fullfil

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+            <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
